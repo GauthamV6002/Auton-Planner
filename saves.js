3e27ebdd97b2654e70605cbeaf5445491dab9150
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2023-10-14 12:56:50 -0600

    add save + fix

--- saves.js
+++ saves.js
@@ -8,3 +8,20 @@ addAutonPoint(108.43, 72.26);
 addAutonPoint(76.68, 47.8);
 addAutonPoint(122.23, 66.79);
 addAutonPoint(74.86, 17.87);
+
+// Other
+
+addAutonPoint(108.52, 14.34);
+addAutonPoint(102.53, 51.79);
+addAutonPoint(98.23, 65.84);
+addAutonPoint(119.85, 71.22);
+addAutonPoint(110.23, 70.96);
+addAutonPoint(81.10, 72);
+addAutonPoint(120.11, 70.96);
+addAutonPoint(110.49, 70.96);
+addAutonPoint(81.10, 51.19);
+addAutonPoint(118.55, 71.22);
+addAutonPoint(110.23, 71.48);
+addAutonPoint(107.63, 14.52);
+addAutonPoint(73.82, 12.7);
+
